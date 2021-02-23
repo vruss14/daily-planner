71a7d2f2c73ae5f66e423e193649e9a2b5138f11
Valerie Russell <vruss14@gmail.com>
2021-02-22 20:06:13 -0500

    fixed an issue where time blocks in the future had the wrong background color

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ let endWorkDay = document.getElementById("17");
 
 let rowArray = [beginWorkDay, tenMorning, eleven, noon, oneAfternoon, twoAfternoon, threeAfternoon, fourAfternoon, endWorkDay];
 
+currentTime = 16;
+
 if (currentTime < 9) {
     for (let i = 0; i < rowArray.length; i++) {
         rowArray[i].setAttribute("class", "row future");
@@ -61,25 +63,38 @@ if (currentTime < 9) {
     console.log("1 to 2 block")
 } else if (currentTime >= 14 && currentTime < 15) {
     for (let i = 0; i < rowArray.length; i++) {
+        rowArray[0].setAttribute("class", "row past");
+        rowArray[1].setAttribute("class", "row past");
+        rowArray[2].setAttribute("class", "row past");
+        rowArray[3].setAttribute("class", "row past");
+        rowArray[4].setAttribute("class", "row past");
         rowArray[5].setAttribute("class", "row present");
-        rowArray[6].setAttribute("class", "row future");
-        rowArray[7].setAttribute("class", "row future");
-        rowArray[8].setAttribute("class", "row future");
-        rowArray[i].setAttribute("class", "row past");
+        rowArray[i].setAttribute("class", "row future");
     }
     console.log("2 to 3 block");
 } else if (currentTime >= 15 && currentTime < 16) {
     for (let i = 0; i < rowArray.length; i++) {
+        rowArray[0].setAttribute("class", "row past");
+        rowArray[1].setAttribute("class", "row past");
+        rowArray[2].setAttribute("class", "row past");
+        rowArray[3].setAttribute("class", "row past");
+        rowArray[4].setAttribute("class", "row past");
+        rowArray[5].setAttribute("class", "row past");
         rowArray[6].setAttribute("class", "row present");
-        rowArray[7].setAttribute("class", "row future");
-        rowArray[8].setAttribute("class", "row future");
-        rowArray[i].setAttribute("class", "row past");
+        rowArray[i].setAttribute("class", "row future");
     }
     console.log("3 to 4 block");
 } else if (currentTime >= 16 && currentTime < 17) {
     for (let i = 0; i < rowArray.length; i++) {
+        rowArray[0].setAttribute("class", "row past");
+        rowArray[1].setAttribute("class", "row past");
+        rowArray[2].setAttribute("class", "row past");
+        rowArray[3].setAttribute("class", "row past");
+        rowArray[4].setAttribute("class", "row past");
+        rowArray[5].setAttribute("class", "row past");
+        rowArray[6].setAttribute("class", "row past");
         rowArray[7].setAttribute("class", "row present");
-        rowArray[i].setAttribute("class", "row past");
+        rowArray[i].setAttribute("class", "row future");
     }
     console.log("4 to 5 block");
 } else if (currentTime >= 17) {
