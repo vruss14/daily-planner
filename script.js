da29d5fa0edaf42919b7b4fcefc818d3a1318dc7
Valerie Russell <vruss14@gmail.com>
2021-02-23 17:16:29 -0500

    added code to store each textarea value locally

--- script.js
+++ script.js
@@ -16,8 +16,7 @@ let endWorkDay = document.getElementById("17");
 
 let rowArray = [beginWorkDay, tenMorning, eleven, noon, oneAfternoon, twoAfternoon, threeAfternoon, fourAfternoon, endWorkDay];
 
-
-// Conditional logic for the background color of each time block depending on time of day
+// Conditional logic for the background color of each time block
 
 if (currentTime < 9) {
     for (let i = 0; i < rowArray.length; i++) {
@@ -120,34 +119,189 @@ if (currentTime < 9) {
 
 // Local storage
 
+// Storing events at 9 AM
+
 let saveNine = document.getElementById("btn-9");
-saveNine.addEventListener("click", storeResponse);
+
+saveNine.addEventListener("click", function () {
+    let textNine = document.getElementById("text-9").value;
+    console.log(textNine);
+    localStorage.setItem("savedevent9", textNine);
+});
+
+function retrieveEventNine () {
+    let preserveNine = localStorage.getItem("savedevent9");
+    let writtenEvent9 = document.getElementById("text-9");
+    writtenEvent9.textContent = preserveNine;
+    console.log(preserveNine);
+}
+
+retrieveEventNine();
+
+
+// Storing events at 10 AM
 
 let saveTen = document.getElementById("btn-10");
-saveTen.addEventListener("click", storeResponse);
+
+saveTen.addEventListener("click", function () {
+    let textTen = document.getElementById("text-10").value;
+    console.log(textTen);
+    localStorage.setItem("savedevent10", textTen);
+});
+
+function retrieveEventTen () {
+    let preserveTen = localStorage.getItem("savedevent10");
+    let writtenEvent10 = document.getElementById("text-10");
+    writtenEvent10.textContent = preserveTen;
+    console.log(preserveTen);
+}
+
+retrieveEventTen();
+
+
+// Storing events at 11 AM
 
 let saveEleven = document.getElementById("btn-11");
-saveEleven.addEventListener("click", storeResponse);
 
-let saveNoon = document.getElementById("btn-12");
-saveNoon.addEventListener("click", storeResponse);
+saveEleven.addEventListener("click", function () {
+    let textEleven = document.getElementById("text-11").value;
+    console.log(textEleven);
+    localStorage.setItem("savedevent11", textEleven);
+});
 
-let saveOne = document.getElementById("btn-13");
-saveOne.addEventListener("click", storeResponse);
+function retrieveEventEleven () {
+    let preserveEleven = localStorage.getItem("savedevent11");
+    let writtenEvent11 = document.getElementById("text-11");
+    writtenEvent11.textContent = preserveEleven;
+    console.log(preserveEleven);
+}
 
-let saveTwo = document.getElementById("btn-14");
-saveTwo.addEventListener("click", storeResponse);
-
-let saveThree = document.getElementById("btn-15");
-saveThree.addEventListener("click", storeResponse);
-
-let saveFour = document.getElementById("btn-16");
-saveFour.addEventListener("click", storeResponse);
-
-let saveFive = document.getElementById("btn-17");
-saveFive.addEventListener("click", storeResponse);
+retrieveEventEleven();
 
 
-function storeResponse () {
-    console.log("It works!");
+//Storing events at noon
+
+let saveTwelve = document.getElementById("btn-12");
+
+saveTwelve.addEventListener("click", function () {
+    let textTwelve = document.getElementById("text-12").value;
+    console.log(textTwelve);
+    localStorage.setItem("savedevent12", textTwelve);
+});
+
+function retrieveEventTwelve () {
+    let preserveTwelve = localStorage.getItem("savedevent12");
+    let writtenEvent12 = document.getElementById("text-12");
+    writtenEvent12.textContent = preserveTwelve;
+    console.log(preserveTwelve);
+}
+
+retrieveEventTwelve();
+
+
+
+// Storing events at 1 PM
+
+let saveThirteen = document.getElementById("btn-13");
+
+saveThirteen.addEventListener("click", function () {
+    let textThirteen = document.getElementById("text-13").value;
+    console.log(textThirteen);
+    localStorage.setItem("savedevent13", textThirteen);
+});
+
+function retrieveEventThirteen () {
+    let preserveThirteen = localStorage.getItem("savedevent13");
+    let writtenEvent13 = document.getElementById("text-13");
+    writtenEvent13.textContent = preserveThirteen;
+    console.log(preserveThirteen);
+}
+
+retrieveEventThirteen();
+
+
+// Storing events at 2 PM
+
+let saveFourteen = document.getElementById("btn-14");
+
+saveFourteen.addEventListener("click", function () {
+    let textFourteen = document.getElementById("text-14").value;
+    console.log(textFourteen);
+    localStorage.setItem("savedevent14", textFourteen);
+});
+
+function retrieveEventFourteen () {
+    let preserveFourteen = localStorage.getItem("savedevent14");
+    let writtenEvent14 = document.getElementById("text-14");
+    writtenEvent14.textContent = preserveFourteen;
+    console.log(preserveFourteen);
+}
+
+retrieveEventFourteen();
+
+
+
+// Storing events at 3 PM
+
+let saveFifteen = document.getElementById("btn-15");
+
+saveFifteen.addEventListener("click", function () {
+    let textFifteen = document.getElementById("text-15").value;
+    console.log(textFifteen);
+    localStorage.setItem("savedevent15", textFifteen);
+});
+
+function retrieveEventFifteen () {
+    let preserveFifteen = localStorage.getItem("savedevent15");
+    let writtenEvent15 = document.getElementById("text-15");
+    writtenEvent15.textContent = preserveFifteen;
+    console.log(preserveFifteen);
+}
+
+retrieveEventFifteen();
+
+
+// Storing events at 4 PM
+
+let saveSixteen = document.getElementById("btn-16");
+
+saveSixteen.addEventListener("click", function () {
+    let textSixteen = document.getElementById("text-16").value;
+    console.log(textSixteen);
+    localStorage.setItem("savedevent16", textSixteen);
+});
+
+function retrieveEventSixteen () {
+    let preserveSixteen = localStorage.getItem("savedevent16");
+    let writtenEvent16 = document.getElementById("text-16");
+    writtenEvent16.textContent = preserveSixteen;
+    console.log(preserveSixteen);
+}
+
+retrieveEventSixteen();
+
+
+// Storing events at 5 PM
+
+let saveSeventeen = document.getElementById("btn-17");
+
+saveSeventeen.addEventListener("click", function () {
+    let textSeventeen = document.getElementById("text-17").value;
+    console.log(textSeventeen);
+    localStorage.setItem("savedevent17", textSeventeen);
+});
+
+function retrieveEventSeventeen () {
+    let preserveSeventeen = localStorage.getItem("savedevent17");
+    let writtenEvent17 = document.getElementById("text-17");
+    writtenEvent17.textContent = preserveSeventeen;
+    console.log(preserveSeventeen);
+}
+
+retrieveEventSeventeen();
+
+// Clear local storage at the end of each day
+
+if (currentTime == 24) {
+    window.localStorage.clear();
 }
